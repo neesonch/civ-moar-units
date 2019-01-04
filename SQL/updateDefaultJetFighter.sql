55b5f6e89c8ce5fb394af2805d7091270cbd9577
Cormac Neeson <neesonch@tcd.ie>
2019-01-04 20:01:28 +0000

    feat: add mod dependencies, add relative cost and CS for new units, fix path to script for updating vanilla Jet Fighter

--- SQL/updateDefaultJetFighter.sql
+++ SQL/updateDefaultJetFighter.sql
@@ -1,4 +1,4 @@
 
 UPDATE Units
-SET Special = ('SPECIALUNIT_STEALTH'), Description = ('Air Superiority Fighter')
-WHERE Class = 'UNITCLASS_JET_FIGHTER'
+SET Special = 'SPECIALUNIT_STEALTH', Description = 'Air Superiority Fighter'
+WHERE Class = 'UNITCLASS_JET_FIGHTER';
